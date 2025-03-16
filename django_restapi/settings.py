c38cc2a8c27d8064ee92c221d2bbd69714eb7e5e
Cicero Caze <cicerocaze@gmail.com>
2025-03-16 03:03:59 +0000

    feat(settings): move settings sensitive data to .env file

--- django_restapi/settings.py
+++ django_restapi/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv, dotenv_values
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g6inpew7m59vs6+i@q3*b&9n7^mg8wcri60t7nk@q&7w8cqv=b'
+SECRET_KEY = os.getenv('SECRET_KEY') if os.getenv('SECRET_KEY') else None
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
