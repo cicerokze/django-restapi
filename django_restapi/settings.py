42e4570f892b2e73e8b182bc36045353fe7572a9
Cicero Caze <cicerocaze@gmail.com>
2025-03-18 02:55:04 +0000

    fix(settings): fix typing error

--- django_restapi/settings.py
+++ django_restapi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SESECRET_KEY = os.environ.get(
+SECRET_KEY = os.environ.get(
     "DJANGO_SECRET_KEY",
     default=secrets.token_urlsafe(nbytes=64),
 )
