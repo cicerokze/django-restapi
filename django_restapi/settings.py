eb8c0f35726a663f7c0a791e51dbae9490e84c9e
Cicero Caze <cicerocaze@gmail.com>
2025-03-18 03:23:13 +0000

    fix(settings): not loading css files problem in stage

--- django_restapi/settings.py
+++ django_restapi/settings.py
@@ -17,7 +17,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -178,6 +177,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
